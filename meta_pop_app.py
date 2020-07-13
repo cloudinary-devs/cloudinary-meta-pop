72ec181b65c18b90c230fdd20eda9bd4b98b7026
Tal Knopf <tal.knopf@cloudinary.com>
2020-07-13 16:53:17 +0300

    added cleanup method to the app along with consuming variable for custom devider

--- meta_pop_app.py
+++ meta_pop_app.py
@@ -7,28 +7,42 @@ import cloudinary
 import cloudinary.api
 import cloudinary.uploader
 
+import logging
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 # @retry(tries=5, delay=2)
 def inbound_parse():
-    print(json.dumps(request.json))
     payload = request.json
-    metadata_string =payload['original_filename'].split('__')
+    print("Now processing \n" ,json.dumps(payload))
     #add security layer to match password
 
-    #take split logic from var
+    #TODO - take split logic from var
+    #split original filename to metadata + just the filename
+    metadata_string = payload['original_filename'].split(filter_divider)[0]
 
-    #update resource via update api
-    # asset = cloudinary.api.resource(payload['public_id'])
-    result = cloudinary.uploader.update_metadata(metadata_string, payload['public_id'])
-    print(result)
+    #split the public_id to derive the original public_if
+    filtered_assets_name = payload['public_id'].split(filter_divider)[1]
+    
+    #update resource metadata fields
+    meta_result = cloudinary.uploader.update_metadata(metadata_string, payload['public_id'])
+    logging.info(result)
+    
+    if 'Yes' == cleanup_publicid:
+        logging.info("Will cleanup the public id and set it to: ", filtered_assets_name)
+        rename_results = cloudinary.uploader.rename(payload['public_id'], filtered_assets_name)
+    
+    
     return "OK"
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
-    upload_preset = str(os.environ.get('UP_PRESET', 'email_uploader'))
+    
     cld_url = str(os.environ.get('CLOUDINARY_URL', '')) 
+    filter_divider = str(os.environ.get('FILTER_DIVIDER', '__'))
+    cleanup_publicid = str(os.environ.get('CLEANUP_PUBLICID', 'Yes'))
+
     print ("app will run on port:", port)
     app.run(host='0.0.0.0', port=port)
